8b65d09d1fafe8d11d92a9bb32ffdaadd24478dd
Aminelamallem <baji.lamallem@gmail.com>
2025-01-17 16:20:05 +0100

    exercice 4

---
+++ Exercice.3/exe.js
@@ -0,0 +1,21 @@
+                        
+                        
+                        
+                        
+                        
+                        let gender = 'homme'
+                        let height = '171cm'
+                        let age = 23
+                        console.log(
+    'J`ai ' + age + ',' + ' je mesure ' + height + ' et je suis un homme'
+                        )
+
+
+
+
+
+
+
+
+
+                        
