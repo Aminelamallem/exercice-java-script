9665312746872a9fc484dc072102417cf730f90a
Aminelamallem <baji.lamallem@gmail.com>
2025-01-17 14:29:31 +0100

    first commit

---
+++ Exercice1/exe1.js
@@ -0,0 +1,15 @@
+let nb1 =20;
+let nb2 =20;
+if(nb1<nb2){
+    console.log('nb1 est le monbre le plus petit')
+}
+else if(nb1>nb2){
+    console.log("nb2 est le nombre le plus petit")
+}
+else{
+    console.log("cest les meme")
+}
+
+
+
+
